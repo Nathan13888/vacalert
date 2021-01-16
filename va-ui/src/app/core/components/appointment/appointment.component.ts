6fd0341d252fe162f04bdd185c468010304f031e
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2021-01-16 17:40:10 -0500

    update
    Signed-off-by: Nathan13888 <29968201+Nathan13888@users.noreply.github.com>

--- va-ui/src/app/core/components/appointment/appointment.component.ts
+++ va-ui/src/app/core/components/appointment/appointment.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Location, LocationControllerService } from '@app/api';
+import { AppStateService } from '@app/core/services/app-state.service';
 import { NavToolbarService } from '@app/core/services/nav-toolbar.service';
+import { NavigationService } from '@app/core/services/navigation.service';
 import { Formatter } from '@app/shared/common/formatter';
-import { GeoUtils } from '@app/shared/common/geo-utils';
 import { BaseComponent } from '@app/shared/components/base/base.component';
-import { takeUntil } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-appointment',
@@ -12,14 +15,26 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./appointment.component.css'],
 })
 export class AppointmentComponent extends BaseComponent {
-  appointmentTime: string;
   loc: Location;
 
   formatter = Formatter;
 
+  @ViewChild('locationInput') locationInput: ElementRef;
+
+  get appointmentTime() {
+    return this.appStateService.appointment.time;
+  }
+
+  set appointmentTime(time: string) {
+    this.appStateService.appointment.time = time;
+  }
+
   constructor(
+    private activatedRoute: ActivatedRoute,
     private toolbarService: NavToolbarService,
-    private locationControllerService: LocationControllerService
+    private locationControllerService: LocationControllerService,
+    private appStateService: AppStateService,
+    private navigationService: NavigationService
   ) {
     super();
   }
@@ -28,15 +43,35 @@ export class AppointmentComponent extends BaseComponent {
     const toolbar = this.toolbarService.defaultInstance();
     toolbar.enableAppointment = false;
 
-    this.appointmentTime = '2021-02-15T10:00:00';
+    //this.appointmentTime = '2021-02-15T10:00:00';
 
-    this.locationControllerService
-      .locationControllerFind()
-      .pipe(takeUntil(this.ngUnsubscribe))
-      .subscribe((locs: Location[]) => {
-        const [lat, lng] = [43.5947768, -79.7162122];
-        GeoUtils.sortByDistance(lat, lng, locs);
-        if (locs.length > 0) this.loc = locs[0];
+    this.activatedRoute.paramMap
+      .pipe(
+        switchMap((params) => {
+          const locId = params.get('loc');
+          if (locId) {
+            return this.locationControllerService.locationControllerFindById(
+              locId
+            );
+          } else return of(undefined);
+        }),
+        takeUntil(this.ngUnsubscribe)
+      )
+      .subscribe((loc: Location | undefined) => {
+        this.loc = loc;
       });
+  }
+
+  onEnterLocation() {
+    this.loc = undefined;
+    setTimeout(() => {
+      if (this.locationInput) {
+        this.locationInput.nativeElement.focus();
+      }
+    }, 200);
+  }
+
+  onPickLocation() {
+    this.navigationService.navigate('/locator');
   }
 }
--- va-ui/src/app/core/services/app-state.service.ts
+++ va-ui/src/app/core/services/app-state.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { UserProfile } from '@app/api/model/userProfile';
 
+export interface Appointment {
+  time?: string;
+  locationId?: string;
+  customLocation?: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AppStateService {
   userProfile: UserProfile;
+
+  appointment: Appointment = {};
 
   constructor() {}
 }
